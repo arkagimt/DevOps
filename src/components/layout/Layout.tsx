bc13e2dd77ef15ca5ed656d1948e8f99d2ce70e3
garka009 <garka009@gmail.com>
2025-12-04 23:33:39 +0530

    Hide sidebar on overview page for immersive experience

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation } from 'react-router-dom';
 import Sidebar from './Sidebar';
 
 const Layout: React.FC = () => {
+    const location = useLocation();
+    const isOverviewPage = location.pathname === '/';
+
     return (
         <div className="flex h-screen bg-slate-950 text-slate-200 overflow-hidden font-sans">
-            <Sidebar />
+            {!isOverviewPage && <Sidebar />}
             <main className="flex-1 h-full overflow-hidden relative">
                 <Outlet />
             </main>
